7e1ffe9111371a80e7926ce198952d48b88d8aba
Bilal Ahmad Bhat <crediblebilal@gmail.com>
2022-01-16 12:48:53 +0530

    first commit

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="container" style="max-width: 600px">
+    <h2 class="text-center mt-5">Vue Todo App</h2>
+
+    <div class="d-flex mt-5">
+      <input
+        type="text"
+        v-model="task"
+        placeholder="Enter task"
+        class="w-100 form-control"
+      />
+      <button class="btn btn-warning rounded-0" @click="submitTask">
+        SUBMIT
+      </button>
+    </div>
+
+    <table class="table table-bordered mt-5">
+      <thead>
+        <tr>
+          <th scope="col">Task</th>
+          <th scope="col" style="width: 120px">Status</th>
+          <th scope="col" class="text-center"  >#</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(task, index) in tasks" :key="index">
+          <td>
+            <span> {{ task.name }} </span>
+          </td>
+          <td>
+            <span
+              class="text-danger"
+            >
+              {{ capitalizeFirstChar(task.status) }}
+            </span>
+          </td>
+          <td class="text-center">
+            <div @click="deleteTask(index)">
+              <span class="fa fa-trash pointer"></span>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoApp",
+
+  data() {
+    return {
+      task: "",
+
+      tasks: [
+        {
+          name: "Task One.",
+          status: "todo",
+        },
+        {
+          name: "Task Two.",
+          status: "todo",
+        },
+        {
+          name: "Task Three",
+          status: "todo",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    capitalizeFirstChar(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    },
+
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+
+    submitTask() {
+      if (this.task.length === 0) return;
+      else {
+        this.tasks.push({
+          name: this.task,
+          status: "todo",
+        });
+      }
+
+      this.task = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.pointer{
+  cursor: pointer;
+}
+</style>
